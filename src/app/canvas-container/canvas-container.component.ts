917408500ac3c286e0c766d6e25ac5a7ffea5e6e
pikr-fra-RTE <franck.picard_externe@rte-france.com>
2025-02-26 17:21:25 +0100

    add proto-component for 3D line with camera control
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>

---
+++ src/app/canvas-container/canvas-container.component.ts
@@ -0,0 +1,94 @@
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, viewChild } from '@angular/core';
+
+import * as THREE from 'three';
+
+@Component({
+  selector: 'app-canvas-container',
+  imports: [],
+  templateUrl: './canvas-container.component.html',
+  styleUrl: './canvas-container.component.scss'
+})
+export class CanvasContainerComponent implements OnInit {
+  canvasEl = viewChild<ElementRef<HTMLCanvasElement>>('canvasBox');
+
+  ngOnInit (): void {
+    console.log(this.canvasEl()?.nativeElement)
+    this.createThreeJsBox();
+  }
+
+  createThreeJsBox(): void {
+    const canvas = this.canvasEl()?.nativeElement;
+    const scene = new THREE.Scene();
+    const material = new THREE.MeshToonMaterial();
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 0.5);
+    pointLight.position.x = 2;
+    pointLight.position.y = 2;
+    pointLight.position.z = 2;
+    scene.add(pointLight);
+
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(1.5, 1.5, 1.5),
+      material
+    );
+
+    scene.add(box);
+
+    const canvasSizes = {
+      width: 800,
+      height: 600,
+    };
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      canvasSizes.width / canvasSizes.height,
+      0.001,
+      1000
+    );
+    camera.position.z = 30;
+    scene.add(camera);
+
+    if (!canvas) {
+      return;
+    }
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    renderer.setClearColor(0xe232222, 1);
+    renderer.setSize(canvasSizes.width, canvasSizes.height);
+
+    window.addEventListener('resize', () => {
+      canvasSizes.width = window.innerWidth;
+      canvasSizes.height = window.innerHeight;
+
+      camera.aspect = canvasSizes.width / canvasSizes.height;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(canvasSizes.width, canvasSizes.height);
+      renderer.render(scene, camera);
+    });
+
+    const clock = new THREE.Clock();
+
+    const animateGeometry = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Update animation objects
+      box.rotation.x = elapsedTime;
+      box.rotation.y = elapsedTime;
+      box.rotation.z = elapsedTime;
+
+      // Render
+      renderer.render(scene, camera);
+
+      // Call animateGeometry again on the next frame
+      window.requestAnimationFrame(animateGeometry);
+    };
+
+    animateGeometry();
+  }
+}
---
+++ src/app/line-drawing/line-drawing.model.ts
@@ -0,0 +1,5 @@
+export interface VerticesCoordinate {
+  x: number,
+  y: number,
+  z: number
+}
