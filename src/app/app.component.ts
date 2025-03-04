fb2c2b1b479f5357060dedd16c0b3340c18241c9
martin JOZAN <martin.jozan_externe@rte-france.com>
2025-03-04 15:24:07 +0100

    feat: Set up pyodide and deploy
    Signed-off-by: martin JOZAN <martin.jozan_externe@rte-france.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { loadPyodide } from "pyodide";
+import pythonScript from "./python/example.py";
+import importScript from "./python/imports.py";
+import { CommonModule } from "@angular/common";
+
+const initPyodide = async () => {
+  console.log("Loading Pyodide...");
+  let root = window.location.origin;
+  // if (root.includes("github.io") || root.includes("localhost")) {
+  const repoName = window.location.href.split("/")[3];
+  root = window.location.origin + "/" + repoName;
+  // }
+  console.log("indexURL:", root + "/pyodide");
+  return loadPyodide({
+    indexURL: root + "/pyodide/",
+    // lockFileURL: root + "/pyodide/pyodide-lock.json",
+    packages: ["mechaphlowers"],
+  });
+};
+
+type PyodideAPI = Awaited<ReturnType<typeof initPyodide>>;
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'phlowers-stellar-app';
+export class AppComponent implements OnInit {
+  title = "phlowers-stellar-app";
+  pyodideLoadTime = 0;
+  importTime = 0;
+  runTime = 0;
+  pyodide: PyodideAPI | null = null;
+
+  run() {
+    const start = performance.now();
+    this.pyodide?.runPython(pythonScript, {});
+    this.runTime = performance.now() - start;
+  }
+
+  async ngOnInit() {
+    const start = performance.now();
+    this.pyodide = await initPyodide();
+    this.pyodideLoadTime = performance.now() - start;
+    this.pyodide.runPython(importScript, {});
+    this.importTime = performance.now() - this.pyodideLoadTime;
+  }
 }
