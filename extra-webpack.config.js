a35628129b7677855a08ffccdd8cde1ec4d769c2
martin JOZAN <martin.jozan_externe@rte-france.com>
2025-03-05 22:34:05 +0100

    feat: Set up worker and fetch parts of packages in CDN
    Signed-off-by: martin JOZAN <martin.jozan_externe@rte-france.com>

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,4 +1,6 @@
 const { PyodidePlugin } = require("@pyodide/webpack-plugin");
+// const CompressionPlugin = require(`compression-webpack-plugin`);
+// const BrotliPlugin = require(`brotli-webpack-plugin`);
 
 module.exports = {
   module: {
@@ -9,7 +11,22 @@ module.exports = {
       },
     ],
   },
-  plugins: [new PyodidePlugin()],
+  plugins: [
+    // new BrotliPlugin({
+    //   asset: "[fileWithoutExt].[ext].br",
+    //   test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
+    // }),
+    // new CompressionPlugin({
+    //   test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
+    //   filename(info) {
+    //     let opFile = info?.filename?.split("."),
+    //       opFileType = opFile?.pop(),
+    //       opFileName = opFile?.join(".");
+    //     return `${opFileName}.${opFileType}.gzip`;
+    //   },
+    // }),
+    new PyodidePlugin(),
+  ],
   externals: {
     "node:fs": "{}",
     "node:path": "{}",
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,7 @@ import pythonScript from "./python/example.py";
 import importScript from "./python/imports.py";
 import { CommonModule } from "@angular/common";
 
-const initPyodide = async () => {
-  console.log("Loading Pyodide...");
-  let root = window.location.origin;
-  // if (root.includes("github.io") || root.includes("localhost")) {
-  const repoName = window.location.href.split("/")[3];
-  root = window.location.origin + "/" + repoName;
-  // }
-  console.log("indexURL:", root + "/pyodide");
-  return loadPyodide({
-    indexURL: root + "/pyodide/",
-    // lockFileURL: root + "/pyodide/pyodide-lock.json",
-    packages: ["mechaphlowers"],
-  });
-};
-
-type PyodideAPI = Awaited<ReturnType<typeof initPyodide>>;
+type PyodideAPI = Awaited<ReturnType<any>>;
 
 @Component({
   selector: "app-root",
@@ -30,22 +15,34 @@ type PyodideAPI = Awaited<ReturnType<typeof initPyodide>>;
 })
 export class AppComponent implements OnInit {
   title = "phlowers-stellar-app";
-  pyodideLoadTime = 0;
+  loadTime = 0;
   importTime = 0;
   runTime = 0;
-  pyodide: PyodideAPI | null = null;
+  pythonWorker: Worker | null = null;
+
+  constructor() {
+    this.pythonWorker = this.setupWorker();
+  }
+
+  setupWorker() {
+    const worker = new Worker(new URL("./app.worker", import.meta.url), {
+      name: window.location.href,
+    });
+    worker.onmessage = ({ data }) => {
+      if (data.loadTime) {
+        this.loadTime = data.loadTime;
+      } else if (data.importTime) {
+        this.importTime = data.importTime;
+      } else if (data.runTime) {
+        this.runTime = data.runTime;
+      }
+    };
+    return worker;
+  }
 
   run() {
-    const start = performance.now();
-    this.pyodide?.runPython(pythonScript, {});
-    this.runTime = performance.now() - start;
+    this.pythonWorker?.postMessage(4);
   }
 
-  async ngOnInit() {
-    const start = performance.now();
-    this.pyodide = await initPyodide();
-    this.pyodideLoadTime = performance.now() - start;
-    this.pyodide.runPython(importScript, {});
-    this.importTime = performance.now() - this.pyodideLoadTime;
-  }
+  async ngOnInit() {}
 }
