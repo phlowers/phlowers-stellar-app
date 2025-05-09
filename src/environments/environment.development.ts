07d5ca1dff33d69c67fe15166badcb4674e785fd
martin JOZAN <martin.jozan_externe@rte-france.com>
2025-05-09 16:07:37 +0200

    feat: Set version and build time
    Signed-off-by: martin JOZAN <martin.jozan_externe@rte-france.com>

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,5 +6,7 @@
  */
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  version: '{BUILD_VERSION}',
+  buildTime: '{BUILD_TIME}'
 };
