628e2fc3a8ff757313f3c2bc978fbb70ad86daf7
Franck P. <franck.picard_externe@rte-france.com>
2025-06-18 11:44:07 +0200

    add base for card component (#38)
    * add base for card component
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>
    
    * fix tabindex attribution and add unit tests
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>
    
    * add keyboard event for 'button' card
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>
    
    * change keyboard action in parent component consuption
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>
    
    * add back md docs from develop, add custom atomic components doc, custom-properties for elevations, reset homepage for a non debug page
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>
    
    * fix homepage test
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>
    
    ---------
    
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>

--- src/app/ui/pages/home/home.component.spec.ts
+++ src/app/ui/pages/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,19 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => 'test'
+              }
+            }
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IconComponent } from '../../shared/components/atoms/icon/icon.component';
+import { ButtonComponent } from '../../shared/components/atoms/button/button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html'
+  imports: [RouterLink, ButtonComponent, IconComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {}
