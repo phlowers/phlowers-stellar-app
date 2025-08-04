3b52377f2c67a8a4cb685b05ef8506c755e1824a
Franck P. <franck.picard_externe@rte-france.com>
2025-08-04 11:28:54 +0200

    quick fix for accessibility issues and refactor for primeng-preset and primeNg components overrides (#99)
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>

--- src/app/ui/pages/404/404.component.ts
+++ src/app/ui/pages/404/404.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ButtonComponent } from '@ui/shared/components/atoms/button/button.component';
 
 @Component({
   selector: 'app-404',
+  imports: [ButtonComponent],
   templateUrl: './404.component.html'
 })
 export class NotFoundComponent {
-  constructor(private router: Router) {}
+  constructor(private readonly router: Router) {}
 
   goToHome() {
     this.router.navigate(['/']);
