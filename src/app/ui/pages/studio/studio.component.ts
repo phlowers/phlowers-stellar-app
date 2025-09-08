1262a6917b973e42d782a3dfbeb6e53829bd9cdd
martin JOZAN <martin.jozan_externe@rte-france.com>
2025-09-08 11:42:46 +0200

    --wip-- [skip ci]

--- src/app/ui/pages/studio/studio.component.ts
+++ src/app/ui/pages/studio/studio.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { StudioComponent } from '../../shared/components/studio/studio.component';
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-studio-page',
-  imports: [StudioComponent],
+  imports: [StudioComponent, NgxSliderModule],
   templateUrl: './studio.component.html',
   styleUrl: './studio.component.scss'
 })
-export class StudioPageComponent {}
+export class StudioPageComponent {
+  minValue = signal(0);
+  maxValue = signal(0);
+  options = signal<Options>({
+    floor: 0,
+    ceil: 1,
+    step: 1,
+    showTicks: true,
+    showTicksValues: true,
+    animate: false,
+    animateOnMove: false
+  });
+}
