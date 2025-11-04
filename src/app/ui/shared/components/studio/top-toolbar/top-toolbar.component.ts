aefe88894e10aad597578c629ee78d24dadac81c
pikr-fra-RTE <franck.picard_externe@rte-france.com>
2025-11-04 14:28:28 +0100

    Add custom multiselect style specific for top toolbar
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>

--- src/app/ui/shared/components/studio/top-toolbar/top-toolbar.component.ts
+++ src/app/ui/shared/components/studio/top-toolbar/top-toolbar.component.ts
@@ -1,18 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { SelectButtonModule } from 'primeng/selectbutton';
 import { FormsModule } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { DividerModule } from 'primeng/divider';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
-import { PlotService } from '@src/app/ui/pages/studio/plot.service';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { PlotService } from '@ui/pages/studio/plot.service';
+import { IconComponent } from '@ui/shared/components/atoms/icon/icon.component';
 
 @Component({
   selector: 'app-studio-top-toolbar',
   templateUrl: './top-toolbar.component.html',
   styleUrl: './top-toolbar.component.scss',
-  imports: [SelectButtonModule, FormsModule, DividerModule, ToggleSwitchModule]
+  imports: [
+    SelectButtonModule,
+    FormsModule,
+    DividerModule,
+    ToggleSwitchModule,
+    MultiSelectModule,
+    IconComponent
+  ]
 })
 export class StudioTopToolbarComponent {
   constructor(public readonly plotService: PlotService) {}
+
   threeDOptions = signal<
     {
       label: string;
@@ -22,6 +32,7 @@ export class StudioTopToolbarComponent {
     { label: '3D', value: '3d' },
     { label: '2D', value: '2d' }
   ]);
+
   sideOptions = signal<
     {
       label: string;
@@ -31,4 +42,19 @@ export class StudioTopToolbarComponent {
     { label: $localize`Profile`, value: 'profile' },
     { label: $localize`Face`, value: 'face' }
   ]);
+
+  displayOptions = signal<
+    {
+      label: string;
+      value: string;
+    }[]
+  >([
+    { label: $localize`Loads`, value: 'loads' },
+    { label: $localize`Obstacles`, value: 'mesh' },
+    { label: $localize`Ground`, value: 'ground' },
+    { label: $localize`Angle en ligne`, value: 'angleInLine' },
+    { label: $localize`Measure`, value: 'measure' }
+  ]);
+
+  displayOptionsStatus = signal<boolean>(false);
 }
