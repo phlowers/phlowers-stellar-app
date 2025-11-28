7d67587458063d1cab893344aaba92353e19b5d4
pikr-fra-RTE <franck.picard_externe@rte-france.com>
2025-11-28 11:05:25 +0100

    change all tangent-bearings refs to tangent-aiming
    Signed-off-by: pikr-fra-RTE <franck.picard_externe@rte-france.com>

---
+++ src/app/ui/pages/studio/tools-dialog/field-measuring/components/calculus-setting/tangent-aiming/tangent-aiming.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tangent-aiming',
+  imports: [],
+  templateUrl: './tangent-aiming.component.html',
+  styleUrl: './tangent-aiming.component.scss'
+})
+export class TangentAimingComponent {}
--- src/app/ui/pages/studio/tools-dialog/field-measuring/components/calculus-setting/tangent-bearings/tangent-bearings.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tangent-bearings',
-  imports: [],
-  templateUrl: './tangent-bearings.component.html',
-  styleUrl: './tangent-bearings.component.scss'
-})
-export class TangentBearingsComponent {}
