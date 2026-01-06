503c97af74fc5e426a7b044e1dde3a43aa4afc53
mjozan-rte <martin.jozan_externe@rte-france.com>
2026-01-06 08:56:49 +0100

    feat: modify result card font size (#397)
    Signed-off-by: mjozan-rte <martin.jozan_externe@rte-france.com>

--- src/app/core/services/worker_python/tasks/python-scripts/functions.py
+++ src/app/core/services/worker_python/tasks/python-scripts/functions.py
@@ -79,6 +79,7 @@ class Support:
     towerModel: Optional[str] = None
     chainLength: Optional[float] = None
     chainWeight: Optional[float] = None
+    towerModel: Optional[str] = None
     chainV: Optional[bool] = None
     counterWeight: Optional[float] = None
     supportFootAltitude: Optional[float] = None
