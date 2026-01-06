b7bb329e29356e5aa935aec858a698a94e3eeced
mjozan-rte <martin.jozan_externe@rte-france.com>
2026-01-06 11:42:39 +0100

    feat: make initial conditions fields compulsory (#398)
    Signed-off-by: mjozan-rte <martin.jozan_externe@rte-france.com>

--- src/app/core/services/worker_python/tasks/python-scripts/functions.py
+++ src/app/core/services/worker_python/tasks/python-scripts/functions.py
@@ -82,6 +82,7 @@ class Support:
     towerModel: Optional[str] = None
     chainV: Optional[bool] = None
     counterWeight: Optional[float] = None
+    towerModel: Optional[str] = None
     supportFootAltitude: Optional[float] = None
     attachmentPosition: Optional[str] = None
     chainSurface: Optional[float] = None
