891faf8f960e5266d8a2f4e4fa099913d65015cb
mjozan-rte <martin.jozan_externe@rte-france.com>
2026-01-12 14:09:22 +0100

    feat: field measuring span preselection (#408)
    Signed-off-by: mjozan-rte <martin.jozan_externe@rte-france.com>

---
+++ src/app/ui/shared/helpers/findMiddleSpan.ts
@@ -0,0 +1,15 @@
+/**
+ * Find the middle span between two supports
+ * @param startSupport - The start support number
+ * @param endSupport - The end support number
+ * @returns The middle span as an array of two numbers
+ */
+export const findMiddleSpan = (
+  startSupport: number,
+  endSupport: number
+): number[] => {
+  return [
+    Math.floor((startSupport + endSupport) / 2),
+    Math.floor((startSupport + endSupport) / 2) + 1
+  ];
+};
