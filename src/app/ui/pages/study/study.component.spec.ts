0fbebcc36696a166b9b61f8ed114a5262c84232d
mjozan-rte <martin.jozan_externe@rte-france.com>
2026-01-20 14:50:32 +0100

    feat: improve guying popin (#433)
    Signed-off-by: mjozan-rte <martin.jozan_externe@rte-france.com>

--- src/app/ui/pages/study/study.component.spec.ts
+++ src/app/ui/pages/study/study.component.spec.ts
@@ -108,7 +108,8 @@ describe('StudyComponent', () => {
     charges: [],
     selected_charge_uuid: null,
     field_measures: [],
-    selected_field_measure_uuid: undefined
+    selected_field_measure_uuid: undefined,
+    vtl_and_guying: undefined
   };
 
   const mockInitialCondition: InitialCondition = {
