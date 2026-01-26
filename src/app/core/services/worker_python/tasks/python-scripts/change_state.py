4b11d937ede4ac02bc377a879b7cbef418902717
WANG Jérémy <jeremy.wang@rte-france.com>
2026-01-26 17:54:49 +0100

    separate punctual and marking
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/app/core/services/worker_python/tasks/python-scripts/change_state.py
+++ src/app/core/services/worker_python/tasks/python-scripts/change_state.py
@@ -47,6 +47,7 @@ def change_state(js_inputs: dict):
 
     punctual_load = change_state_inputs["spanLoads"]
     load_position_list = []
+    load_weight_list = []
     for index, span in enumerate(punctual_load):
         if span['referenceSupport'] == 'LEFT':
             load_position_list.append(span["loadPosition"])
@@ -55,9 +56,14 @@ def change_state(js_inputs: dict):
             load_position_list.append(span_length - span["loadPosition"])
         else:
             load_position_list.append(0)
-    load_position_meters = np.array(load_position_list)
-    load_weight = np.array([span["loadWeight"] for span in punctual_load])
 
+        if span['type'] == 'punctual':
+            load_weight_list.append(span["loadWeight"])
+        else:
+            load_weight_list.append(0)
+            
+    load_position_meters = np.array(load_position_list)
+    load_weight = np.array(load_weight_list)
 
     # Small optimization if no loads
     if (load_position_meters != 0).any() and (load_position_meters != 0).any():
