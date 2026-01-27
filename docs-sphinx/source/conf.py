382226a41145f90de930128fd330253fb1000c23
Pascal Senizergues <pascal.senizergues@rte-france.com>
2026-01-27 16:37:07 +0100

    init sphinx-js
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>

--- docs-sphinx/source/conf.py
+++ docs-sphinx/source/conf.py
@@ -13,26 +13,13 @@ author = 'Adrien Goeller'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_js',
-    'myst_parser',  # Support for Markdown files
-]
+extensions = ['sphinx_js']
 
 # sphinx-js configuration for TypeScript
 js_language = 'typescript'
 js_source_path = '../../src'
-jsdoc_tsconfig_path = '../tsconfig.typedoc.json'
+jsdoc_tsconfig_path = '../../tsconfig.json'
 primary_domain = 'js'
-
-# MyST-Parser configuration for Markdown support
-myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
-]
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -44,27 +31,5 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_material'
-
-# Material theme options
-html_theme_options = {
-    "nav_title": "Stellar",
-    "base_url": "https://stellar.readthedocs.io/",
-    "repo_url": "https://github.com/phlowers/stellar",
-    "repo_name": "Stellar",
-    "google_analytics_account": "",
-    "html_minify": False,
-    "css_minify": False,
-    "globaltoc_depth": 3,
-    "globaltoc_collapse": True,
-    "globaltoc_includehidden": True,
-    "color_primary": "blue",
-    "color_accent": "light-blue",
-}
-
-# Required for sphinx-material
-html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
-}
-
-html_title = "Stellar Documentation"
+html_theme = 'alabaster'
+html_static_path = ['_static']
