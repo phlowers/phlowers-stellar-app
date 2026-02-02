605875d6a773a7eee8668eb3d00506dd0fbdd6b7
WANG Jérémy <jeremy.wang@rte-france.com>
2026-02-02 11:28:07 +0100

    comment
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/app/core/services/worker_python/tasks/python-scripts/change_state.py
+++ src/app/core/services/worker_python/tasks/python-scripts/change_state.py
@@ -60,6 +60,7 @@ def change_state(js_inputs: dict):
         if span['type'] == 'punctual':
             load_weight_list.append(span["loadWeight"])
         else:
+            # Temporary work around to factor in marking
             load_weight_list.append(0.01)
             
     load_position_meters = np.array(load_position_list)
