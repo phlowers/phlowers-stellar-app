e6275a6525dac1b5beb5096f3d4108c6a97e23d4
WANG Jérémy <jeremy.wang@rte-france.com>
2026-02-05 11:05:25 +0100

    update mph renames
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/app/core/services/worker_python/tasks/python-scripts/guying.py
+++ src/app/core/services/worker_python/tasks/python-scripts/guying.py
@@ -16,16 +16,16 @@ def calculate_guying(js_inputs: dict):
     print("python_inputs: ", js_inputs.to_py())
     guying_inputs = GuyingInputs(**js_inputs.to_py())
     guying = Guying(engine)
-    guying_results = guying.get_guying_results_span_view(
-        span_index=guying_inputs.selectedSpanIndex,
+    guying_results = guying.compute(
+        index=guying_inputs.selectedSpanIndex,
         with_pulley=guying_inputs.hasPulley,
-        guying_altitude=guying_inputs.altitude,
-        guying_horizontal_distance=guying_inputs.horizontalDistance,
-        selected_support=guying_inputs.selectedSupport.lower(),
+        altitude=guying_inputs.altitude,
+        horizontal_distance=guying_inputs.horizontalDistance,
+        side=guying_inputs.selectedSupport.lower(),
+        view="span"
     )
-    print(guying_results)
-    print(guying_results.value_dict)
-    # TODO: hard code units?
+
+    # hard code units?
     return {
         "tensionInGuy": guying_results.guying_tension.to("daN").m,
         "guyAngle": guying_results.guying_angle_degrees.to("deg").m,
