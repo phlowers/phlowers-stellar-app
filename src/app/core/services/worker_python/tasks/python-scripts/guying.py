e3e78c63b3e123b82eedd6beb457de98bf625de0
Jérémy Wang <jeremy.wang@rte-france.com>
2026-02-16 10:54:41 +0100

    Guying: link to mechaphlowers (#498)
    * Guying link to mph
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * fix guying side
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * update mph renames
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * remove prints
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * fix test
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * type PlotEngine
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * update mechaphlowers 0.5.3
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    ---------
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/app/core/services/worker_python/tasks/python-scripts/guying.py
+++ src/app/core/services/worker_python/tasks/python-scripts/guying.py
@@ -1,20 +1,34 @@
 from dataclasses import dataclass
+from typing import Literal
+from mechaphlowers.core.models.guying import Guying
 
 
 @dataclass
 class GuyingInputs:
-    support_number: int
     horizontalDistance: float
+    altitude: float
     hasPulley: bool
+    selectedSpanIndex: int
+    selectedSupport: Literal['LEFT' , 'RIGHT']
 
 
 def calculate_guying(js_inputs: dict):
-    python_inputs: GuyingInputs = js_inputs.to_py()
-    print("python_inputs: ", python_inputs)
+    guying_inputs = GuyingInputs(**js_inputs.to_py())
+    guying = Guying(engine)
+    guying_results = guying.compute(
+        index=guying_inputs.selectedSpanIndex,
+        with_pulley=guying_inputs.hasPulley,
+        altitude=guying_inputs.altitude,
+        horizontal_distance=guying_inputs.horizontalDistance,
+        side=guying_inputs.selectedSupport.lower(),
+        view="span"
+    )
+
+    # hard code units?
     return {
-        "tensionInGuy": 101,
-        "guyAngle": 30,
-        "chargeVUnderConsole": 102,
-        "chargeHUnderConsole": 103,
-        "chargeLIfPulley": 104,
+        "tensionInGuy": guying_results.guying_tension.to("daN").m,
+        "guyAngle": guying_results.guying_angle_degrees.to("deg").m,
+        "chargeVUnderConsole": guying_results.vertical_force.to("daN").m,
+        "chargeLIfPulley": guying_results.longitudinal_force.to("daN").m,
+        "chargeHUnderConsole": 0,
     }
