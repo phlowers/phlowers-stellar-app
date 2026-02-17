58f6bcdbb9b775893ab3573ffc9ec36685975535
mjozan-rte <martin.jozan_externe@rte-france.com>
2026-02-17 20:23:36 +0100

    feat: fix test
    Signed-off-by: mjozan-rte <martin.jozan_externe@rte-france.com>
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>

--- src/app/ui/shared/components/studio/studio.component.spec.ts
+++ src/app/ui/shared/components/studio/studio.component.spec.ts
@@ -77,7 +77,13 @@ const mockLitData: GetSectionOutput = {
   load_angle: [],
   displacement: [],
   span_length: [],
-  loads_coords: {}
+  loads_coords: {},
+  parameter: [],
+  tension_sup: [],
+  tension_inf: [],
+  horizontal_distance: [],
+  arc_length: [],
+  T_h: []
 };
 
 describe('StudioComponent', () => {
