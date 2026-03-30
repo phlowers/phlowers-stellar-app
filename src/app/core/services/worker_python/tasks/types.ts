c4bbe1445199873eb27eefd588eebc7984ce41ef
WANG Jérémy <jeremy.wang@rte-france.com>
2026-03-30 15:56:09 +0200

    Revert "cards: slope"
    This reverts commit a1d5fcab104d8a00b57678b79bb864fd2efaba74.
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/app/core/services/worker_python/tasks/types.ts
+++ src/app/core/services/worker_python/tasks/types.ts
@@ -111,10 +111,6 @@ export interface GetSectionOutput {
   elevation: number[];
   /** Cable sag parameter (unitless) at each span */
   parameter: number[];
-  // Slope angle of the left support of the span
-  slope_left: number[];
-  // Slope angle of the right support of the span
-  slope_right: number[];
   /** Superior (upper) tension at each support (daN) */
   tension_sup: number[];
   /** Inferior (lower) tension at each support (daN) */
@@ -127,7 +123,6 @@ export interface GetSectionOutput {
   arc_length: number[];
   /** Horizontal component of cable tension at each span (daN) */
   T_h: number[];
-  // sag S1 and S2
   sag: number[];
   sag_s2: number[];
 }
