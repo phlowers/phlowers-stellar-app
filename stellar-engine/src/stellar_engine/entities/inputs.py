4e17469168d6d4b438feddd77e37178c5739ba78
Jérémy Wang <jeremy.wang@rte-france.com>
2026-03-31 11:27:16 +0200

    Refacto stellar engine scripts part 1 (#670)
    * start regrouping scripts
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * wip migrate scripts
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * wip change_state
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * intermediate refacto state
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * Display sag s1 + s2
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * cards: slope
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * Revert "cards: slope"
    
    This reverts commit a1d5fcab104d8a00b57678b79bb864fd2efaba74.
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * Revert "Display sag s1 + s2"
    
    This reverts commit 50b3df6f59de6cc67727542d3d7ff58bea0a9c1b.
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * prepare new version mph
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * 621 fix page update is no more displaying information (#622)
    
    * fix version check
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix version check
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * Fix copilot review
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    ---------
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * fix invert (#662)
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * Fixbug/610/fix multi bug on charge (#665)
    
    * Fix: mecaphlowers not return 0
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * add button zoom on charge
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * change select span if raw span change
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * fix: bug revert 3d
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * documentation
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * fix return PR copilot
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * good position btn zoom
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    ---------
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * fix bugs of 447 (#664)
    
    * fix bugs of 447
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix bugs of 447 view reset (1a)
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * add obstacle altitude relative to cable attachment
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix failing test
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix fred comment
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    ---------
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * Revert refacto change_state
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * package lock
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * lint
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * copilot review but wip
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    * update set-up-mechaphlowers
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    
    ---------
    
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    Co-authored-by: Pascal SENIZERGUES <67464157+PascalSeniziRTE@users.noreply.github.com>
    Co-authored-by: Fred-RTE <frederic.madranges_externe@rte-france.com>

---
+++ stellar-engine/src/stellar_engine/entities/inputs.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2026, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
+import datetime
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class GuyingInputs:
+    horizontalDistance: float
+    altitude: float
+    hasPulley: bool
+    selectedSpanIndex: int
+    selectedSupport: Literal['LEFT', 'RIGHT']
+
+
+@dataclass
+class ParameterCalibrationInputs:
+    parameterUncertaintyPapoto: float
+    cableTemperatureCalibration: float
+    cableTemperatureCalibrationUncertainty: float
+    parameterPapoto: float
+    span_index: int
+
+
+@dataclass
+class TemperatureCalculationInputs:
+    cableName: str
+    ambientTemperature: float
+    longitude: float
+    latitude: float
+    transit: float
+    skyCover: str
+    altitude: float
+    azimuth: float
+    # datetime.datetime instead?
+    date: datetime.datetime
+    time: datetime.datetime
+    windSpeed: float
+    windSpeedUnit: Literal['kmh', 'ms']
+    windDirection: str
+
+
+@dataclass
+class ClimateCharge:
+    windPressure: float
+    cableTemperature: float
+    symmetryType: str
+    iceThickness: float
+    frontierSupportNumber: float
+    iceThicknessBefore: float
+    iceThicknessAfter: float
+
+
+@dataclass
+class SpanLoad:
+    loadPosition: float
+    loadWeight: float
+
+
+@dataclass
+class ChangeStateInput:
+    climate: ClimateCharge
+    spanLoads: list[SpanLoad]
---
+++ stellar-engine/src/stellar_engine/plot/run_solver.py
@@ -0,0 +1,120 @@
+# Copyright (c) 2026, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
+
+# TODO: investigate to see if logging is imported correctly
+import logging
+
+import numpy as np
+from mechaphlowers import BalanceEngine, PlotEngine, units
+
+from stellar_engine.entities.inputs import ClimateCharge
+from stellar_engine.entities.output import get_coordinates
+
+logger = logging.getLogger("mechaphlowers")
+# Set logger level to WARNING so info messages are shown
+logger.setLevel(logging.WARNING)
+
+
+def apply_span_loads(
+    engine: BalanceEngine, plot_engine: PlotEngine, span_loads: list
+):
+    """Parse span loads and add them to the engine if any are non-zero."""
+    load_position_meters, load_mass = parse_span_loads(engine, span_loads)
+    if (load_position_meters != 0).any() and (load_mass != 0).any():
+        engine.add_loads(load_position_meters, load_mass)
+        # Bug here: plot_engine is not correctly reset
+        plot_engine = plot_engine.generate_reset()
+        # To uncomment with new version of mechaphlowers
+        # plt_line.reset(engine)
+
+
+def parse_span_loads(
+    engine: BalanceEngine, span_loads: list
+) -> tuple[np.ndarray, np.ndarray]:
+    """Convert raw span load dicts into position and mass arrays."""
+    load_position_list = []
+    load_weight_list_daN = []
+    span_lengths = engine.section_array.data["span_length"].to_numpy()
+    for index, span in enumerate(span_loads):
+        try:
+            if span['referenceSupport'] == 'LEFT':
+                load_position_list.append(span["loadPosition"])
+            elif span['referenceSupport'] == 'RIGHT':
+                if 0 <= index < len(span_lengths):
+                    span_length = span_lengths[index]
+                    load_position_list.append(
+                        span_length - span["loadPosition"]
+                    )
+                else:
+                    # use logger instead?
+                    logging.warning(
+                        "Span load index %s is out of bounds for span_length array (size %s). "
+                        "Defaulting load position to 0.",
+                        index,
+                        len(span_lengths),
+                    )
+                    load_position_list.append(0)
+            else:
+                load_position_list.append(0)
+
+            if span['type'] == 'punctual':
+                load_weight_list_daN.append(span["loadWeight"])
+            else:
+                load_weight_list_daN.append(0.01)
+        except KeyError as e:
+            logging.warning(
+                "Span load at index %s is missing required key %s. "
+                "Skipping with defaults (position=0, weight=0.01).",
+                index,
+                e,
+            )
+            load_position_list.append(0)
+            load_weight_list_daN.append(0.01)
+    load_mass_kg = units(load_weight_list_daN, 'daN').to('kg').magnitude
+    return np.array(load_position_list), np.array(load_mass_kg)
+
+
+def change_state(
+    change_state_inputs: dict,
+    engine: BalanceEngine,
+    plot_engine: PlotEngine,
+    base_engine: BalanceEngine,
+    base_plt_line: PlotEngine,
+):
+    # logger.debug("python_inputs: ", str(js_inputs))
+    # print("change_state_inputs", change_state_inputs)
+    climate = ClimateCharge(**change_state_inputs["climate"])
+    # print(change_state_inputs)
+    logger.debug("python_inputs: ", change_state_inputs)
+    wind_pressure = climate.windPressure
+    cable_temperature = climate.cableTemperature
+    ice_thickness = climate.iceThickness / 100  # in meters in the engine
+
+    apply_span_loads(engine, plot_engine, change_state_inputs["spanLoads"])
+
+    engine.solve_adjustment()
+    engine.solve_change_state(
+        ice_thickness=ice_thickness,
+        new_temperature=cable_temperature,
+        wind_pressure=wind_pressure,
+        wind_sense="clockwise",
+    )
+    section_length = len(engine.section_array.data)
+    base_section_length = (
+        len(base_engine.section_array.data) if base_engine else section_length
+    )
+    # TODO: weird consistency base/current engine
+    return {
+        "current": get_coordinates(
+            engine, plot_engine, False, 0, section_length - 1
+        ),
+        "base": get_coordinates(
+            engine, base_plt_line, False, 0, base_section_length - 1
+        )
+        if base_plt_line
+        else None,
+    }
