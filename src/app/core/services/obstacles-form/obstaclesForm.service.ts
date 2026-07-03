ead5f8a4945bf29c07291ccc967f36b8873366e4
Fred-RTE <frederic.madranges_externe@rte-france.com>
2026-07-03 15:45:51 +0200

    fix bug obstacles change span zoom on active span (#1044)
    * fix bug obstacles change span zoom on active span
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * fix returns copilot PR
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    * fix retour sonar
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>
    
    ---------
    
    Signed-off-by: Frédéric Madranges <frederic.madranges_externe@rte-france.com>

--- src/app/core/services/obstacles-form/obstaclesForm.service.ts
+++ src/app/core/services/obstacles-form/obstaclesForm.service.ts
@@ -492,4 +492,15 @@ export class ObstacleFormService {
       this.spanService.spanAmountChoice.set('single');
     }
   }
+
+  syncSpanSelectionWithoutZoom(): void {
+    const supportUuid = this.form.get('supportUuid')?.value;
+    if (!supportUuid) {
+      return;
+    }
+    const supportIndex = this.spanService.getSupportIndex(supportUuid);
+    if (supportIndex >= 0) {
+      this.spanService.spanAmountChoice.set('single');
+    }
+  }
 }
