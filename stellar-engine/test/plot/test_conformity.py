49bb188f5669254fd790b7f1c52044febf5a0153
GOELLER Adrien <adrien.goeller@rte-france.com>
2026-07-13 17:44:42 +0200

    ensure copyright
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- stellar-engine/test/plot/test_conformity.py
+++ stellar-engine/test/plot/test_conformity.py
@@ -1,4 +1,10 @@
-from stellar_engine.core.conformity.scenarios import (
+# Copyright (c) 2026, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
+from stellar_engine.core.conformity.simulation import (
     get_conformity,
 )
 from stellar_engine.plot.obstacles import add_single_obstacle
