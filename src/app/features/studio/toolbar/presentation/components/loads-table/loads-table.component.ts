c8c53cf0264dca8301bccdc10deb00835e72299f
Pascal SENIZERGUES <67464157+PascalSeniziRTE@users.noreply.github.com>
2026-08-25 11:33:32 +0200

    fix decimal display (#1122)
    * fix decimal display
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix copilot review
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix decimal display
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    * fix copilot review
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>
    
    ---------
    
    Signed-off-by: Pascal Senizergues <pascal.senizergues@rte-france.com>

--- src/app/features/studio/toolbar/presentation/components/loads-table/loads-table.component.ts
+++ src/app/features/studio/toolbar/presentation/components/loads-table/loads-table.component.ts
@@ -8,6 +8,7 @@ import {
   TemplateRef,
   viewChild
 } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 import { formatSupportNumber } from '@shared/helpers/formatSupportNumber';
 import { FormsModule } from '@angular/forms';
 import { TranslocoService, TranslocoModule } from '@jsverse/transloco';
@@ -46,6 +47,7 @@ interface SpanLoadRow {
 @Component({
   selector: 'app-loads-table',
   imports: [
+    DecimalPipe,
     IconComponent,
     ButtonComponent,
     ToggleSwitchModule,
